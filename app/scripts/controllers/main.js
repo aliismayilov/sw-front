a4f9c0652bd87c276f9503e4cdc08c70c86ff4e5
Ali Ismayilov <ali@ismailov.info>
2014-04-12 13:51:43 -0300

    Grunt connect proxy setup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('swFrontApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $resource) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
