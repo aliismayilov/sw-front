ad760689a41489216e0a9ea598b6d34602b2fef2
Ali Ismayilov <ali@ismailov.info>
2014-04-13 14:46:47 -0300

    Add more edges

--- app/scripts/services/edges.js
+++ app/scripts/services/edges.js
@@ -5,7 +5,7 @@ angular.module('swFrontApp').service('edges', function() {
     return [
       {
         name: 'Attractive',
-        description: 'blah...',
+        description: 'Attractive...',
         category: {
           name: 'Background'
         },
@@ -15,14 +15,70 @@ angular.module('swFrontApp').service('edges', function() {
         ]
       },
       {
-        name: 'Attractive',
-        description: 'blah...',
+        name: 'Very attractive',
+        description: 'Very attractive...',
         category: {
           name: 'Background'
         },
         requirements: [
           { name: null, value: 'Novice' },
-          { name: 'Vigor', value: 'd6' }
+          { name: null, value: 'Attractive' }
+        ]
+      },
+      {
+        name: 'Brave',
+        description: 'Brave...',
+        category: {
+          name: 'Background'
+        },
+        requirements: [
+          { name: null, value: 'Novice' },
+          { name: 'Spirit', value: 'd6' }
+        ]
+      },
+      {
+        name: 'Block',
+        description: 'Block...',
+        category: {
+          name: 'Combat'
+        },
+        requirements: [
+          { name: null, value: 'Seasoned' },
+          { name: 'Fighting', value: 'd8' }
+        ]
+      },
+      {
+        name: 'Brawler',
+        description: 'Brawler...',
+        category: {
+          name: 'Combat'
+        },
+        requirements: [
+          { name: null, value: 'Novice' },
+          { name: 'Strength', value: 'd6' }
+        ]
+      },
+      {
+        name: 'Command',
+        description: 'Command...',
+        category: {
+          name: 'Leadership'
+        },
+        requirements: [
+          { name: null, value: 'Novice' },
+          { name: 'Smart', value: 'd6' }
+        ]
+      },
+      {
+        name: 'Hold the Line!',
+        description: 'Hold the Line!...',
+        category: {
+          name: 'Leadership'
+        },
+        requirements: [
+          { name: null, value: 'Seasoned' },
+          { name: 'Smart', value: 'd8' },
+          { name: null, value: 'Command' }
         ]
       }
     ];
